f342765bef7997d55886071353fd352e0d41c605
ivan-filzof <ivan.filzof@gmail.com>
2025-08-28 16:59:30 +0800

    -updated favorites function

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './css/App.css';
-import Home from './pages/home';
+import Home from './pages/Home';
 import Favorites from './pages/Favorites';
 import { Routes, Route } from 'react-router-dom';
+import { MovieProvider } from './context/MovieContext';
 import Navbar from './components/NavBar';
 
 function App() {
   return (
-    <div>
+    <MovieProvider>
       <Navbar />
       <main className="main-content">
         <Routes>
@@ -14,7 +15,7 @@ function App() {
           <Route path="/favorites" element={<Favorites />}/>
         </Routes>
       </main>
-    </div>
+    </MovieProvider>
   );
 }
 
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,27 +1,30 @@
 import "../css/MovieCard.css"
+import { useMovieContext } from "../context/MovieContext"
 
 function MovieCard({movie}) {
+    const {isFavorite, addToFavorites, removeFromFavorites} = useMovieContext()
+    const favorite = isFavorite(movie.id)
 
-    function OnFavoriteClick() {
-        alert("Favorite Clicked")        
+    function onFavoriteClick(e) {
+        e.preventDefault()
+        if (favorite) removeFromFavorites(movie.id)
+        else addToFavorites(movie)
     }
-    
-    return (
-      <div className="movie-card">
+
+    return <div className="movie-card">
         <div className="movie-poster">
-          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-          <div className="movie-overlay">
-              <button className="favorite-btn" onClick={OnFavoriteClick}>
-                 ♡   
-              </button>
-          </div>
+            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
+            <div className="movie-overlay">
+                <button className={`favorite-btn ${favorite ? "active" : ""}`} onClick={onFavoriteClick}>
+                    ♥
+                </button>
+            </div>
         </div>
-        <div className="className movie-info">
+        <div className="movie-info">
             <h3>{movie.title}</h3>
             <p>{movie.release_date?.split("-")[0]}</p>
         </div>
-      </div>
-    )
+    </div>
 }
 
-export default MovieCard;
+export default MovieCard
